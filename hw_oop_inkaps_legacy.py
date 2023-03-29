dde11217eb0032daddc4c6a16e29b2154c4d9029
IGNATEV_AA <Artyom-sany4@mail.ru>
2023-03-29 21:58:29 +0300

    смотри в ридми

--- hw_oop_inkaps_legacy.py
+++ hw_oop_inkaps_legacy.py
@@ -16,6 +16,17 @@ class Student:
         else:
             return 'Ошибка'    
         
+    def __str__(self):
+        result = f"""Имя: {self.name} 
+Фамилия: {self.surname} 
+Средняя оценка за домашние задания: {self.grades.values()} 
+Курсы в процессе изучения: {', '.join(self.courses_in_progress)} 
+Завершенные курсы: {', '.join(self.finished_courses)}
+        """
+        return result
+
+a = f''       
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -26,8 +37,12 @@ class Lecturer(Mentor):
     def __init__(self, name, surname, gender):
        self.lect_grades = {}
        self.courses_attached = []
-    
-prep = Lecturer('Имя', 'Фамилие', 'lect_gender')
+
+    def __str__(self):
+        result = f"""Имя: {self.name} 
+Фамилия: {self.surname}
+Средняя оценка за лекции: """
+        return result
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
@@ -38,12 +53,21 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'    
- 
+    
+    def __str__(self):
+        result = f'Имя: {self.name} \nФамилия: {self.surname}'
+        return result
+                
+prep = Lecturer('Имя', 'Фамилие', 'lect_gender')
+                
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
+best_student.courses_in_progress += ['GIT']
+best_student.finished_courses += ['SQL']
  
 cool_reviewer = Reviewer('Some', 'Buddy')
 cool_reviewer.courses_attached += ['Python']
+cool_reviewer.courses_attached += ['GIT']
 
 cool_lecturer = Lecturer('Cool', 'Mentor', 'lect_gender')
 cool_lecturer.courses_attached += ['Python']
@@ -51,9 +75,18 @@ cool_lecturer.courses_attached += ['Python']
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'GIT', 10)
+cool_reviewer.rate_hw(best_student, 'GIT', 8)
 
 best_student.rate_lector(cool_lecturer, 'Python', 9)
+best_student.rate_lector(cool_lecturer, 'JS', 9)
  
 print(best_student.grades)
 print()
 print(cool_lecturer.lect_grades)
+print()
+print(best_student)
+print()
+print(cool_reviewer)
+# print()
+# print(cool_lecturer)
