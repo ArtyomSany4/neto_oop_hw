055b42d39d36a1436b28aaadf5cb2665bf47c301
IGNATEV_AA <Artyom-sany4@mail.ru>
2023-03-29 10:58:12 +0300

    1. Реализована функция выставления студентом оценки Лектору. 2. Остались вопросы к наследованию Лектором списка courses_attached от ментора

--- hw_oop_inkaps_legacy.py
+++ hw_oop_inkaps_legacy.py
@@ -6,6 +6,15 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+
+    def rate_lector(self, lecturer, course, lect_grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.lect_grades:
+                lecturer.lect_grades[course] += [lect_grade]
+            else:
+                lecturer.lect_grades[course] = [lect_grade]
+        else:
+            return 'Ошибка'    
         
 class Mentor:
     def __init__(self, name, surname):
@@ -14,9 +23,11 @@ class Mentor:
         self.courses_attached = []
         
 class Lecturer(Mentor):
-   def __init__(self, name, surname, gender):
+    def __init__(self, name, surname, gender):
        self.lect_grades = {}
+       self.courses_attached = []
     
+prep = Lecturer('Имя', 'Фамилие', 'lect_gender')
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
@@ -33,9 +44,16 @@ best_student.courses_in_progress += ['Python']
  
 cool_reviewer = Reviewer('Some', 'Buddy')
 cool_reviewer.courses_attached += ['Python']
+
+cool_lecturer = Lecturer('Cool', 'Mentor', 'lect_gender')
+cool_lecturer.courses_attached += ['Python']
  
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 cool_reviewer.rate_hw(best_student, 'Python', 10)
+
+best_student.rate_lector(cool_lecturer, 'Python', 9)
  
 print(best_student.grades)
+print()
+print(cool_lecturer.lect_grades)
